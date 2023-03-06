7111089528cfa49e7e3bd14d5c4960748b32da23
AleksNikiforov <35526161+AleksNikiforov@users.noreply.github.com>
2023-03-06 15:45:52 +0500

    Delete algorithms/grokaem directory

--- algorithms/grokaem/recursion.py
+++
@@ -1,8 +0,0 @@
-def countdown(i):
-    print(i)
-    if i < 1:
-        return
-    else:
-        countdown(i - 1)
-
-countdown(10)
--- algorithms/grokaem/recursion_max.py
+++
@@ -1,9 +0,0 @@
-def max(data):
-    max = data[0]
-    for i in range(len(data)):
-        if data[i] > max:
-            max = data[i]
-    return max
-
-
-print(max([1,2,3,4,5,7,8,6,4,3,2]))
